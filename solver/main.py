668bdda0c471eea20c115055ea1837158ccba2b6
Eugene Kokachev <Eugene.Kokachev@gmail.com>
2020-07-14 13:25:01 +1000

    added the check for integer average

--- solver/main.py
+++ solver/main.py
@@ -5,5 +5,10 @@ def check_number_of_tasks(list_of_tasks):
     else:
         return "OK"
 
+def check_integer_average_points(list_of_tasks):
+    if (sum(list_of_tasks) % len(list_of_tasks)) == 0:
+        return "OK"
+    else:
+        return "Failed"
 
 print(check_number_of_tasks([1,2,3]))
--- test/test_solver.py
+++ test/test_solver.py
@@ -1,6 +1,10 @@
 import pytest
 
-from solver.main import check_number_of_tasks
+from solver.main import *
 
 def test_min_3_tasks():
     assert check_number_of_tasks([1,1]) == "Failed"
+
+def test_integer_average_points(): 
+    assert check_integer_average_points([1,1,1]) == "OK"
+    assert check_integer_average_points([1,2,1]) == "Failed"
