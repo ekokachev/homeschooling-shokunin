58dfaa48ffaebd4175da56c9612153b311a9d776
Eugene Kokachev <Eugene.Kokachev@gmail.com>
2020-07-14 20:33:22 +1000

    Test that average can be reached

--- solver/main.py
+++ solver/main.py
@@ -5,7 +5,7 @@ dict = { 10: [{}, {}, {}, {}],
 }
 
 """
-
+import bisect
 
 def check_number_of_tasks(list_of_tasks):
     if len(list_of_tasks) < 3:
@@ -27,7 +27,29 @@ def convert_list_to_dict(list_of_tasks):
     #Convert the list of tasks into appropriate data structure for the next step
     D = {}
     for i in range(len(list_of_tasks)):
-        print(i, list_of_tasks[i])
+        #print(i, list_of_tasks[i])
         D[list_of_tasks[i]] = [set([i])]
     return D
-    
+
+def check_average_is_reachable(D, list_of_tasks): 
+    unique_elements = set(list_of_tasks)
+    queue = list_of_tasks
+    AVERAGE = sum(list_of_tasks) // 3 #floor (integer) division
+    print(AVERAGE)
+    while True: 
+        next_in_line = queue.pop(0)
+        if next_in_line >= AVERAGE:
+            break
+
+        for i in queue:
+            new_element = next_in_line + i
+            if new_element > AVERAGE:
+                break
+            if new_element not in unique_elements:
+                bisect.insort(queue, new_element)
+                unique_elements.add(new_element)         
+
+    if AVERAGE in unique_elements: 
+        return "OK"
+    else:
+        return "Failed"
--- test/test_solver.py
+++ test/test_solver.py
@@ -9,11 +9,17 @@ def test_integer_average_points():
     assert check_integer_average_points([1,1,1]) == "OK"
     assert check_integer_average_points([1,2,1]) == "Failed"
 
-#def test_at_least_one_set_of_tasks_exists():
-#    assert check_average_is_reachable([1,2,3,4,5,7,8]) == "OK"
+def test_average_is_reachable():
+    p1 = convert_list_to_dict([9,9,12])
+    p2 = convert_list_to_dict([9,10,11])
+    p3 = convert_list_to_dict([1,2,3,4,5,7,8])
+    assert check_average_is_reachable(p1, [9,9,12]) == "Failed"
+    assert check_average_is_reachable(p2, [9,10,11]) == "OK"
+    assert check_average_is_reachable(p3, [1,2,3,4,5,7,8]) == "OK"
 
 def test_sort():
     assert sort_list_of_tasks([1,10,1]) == [1,1,10]
 
 def test_convert_list_to_dict():
     assert convert_list_to_dict([3,10,20]) == {3: [{0}], 10: [{1}], 20: [{2}]}
+
