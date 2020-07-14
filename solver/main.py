5f93f973f47587b5d39e4c1596cfb4cda6f92979
Eugene Kokachev <Eugene.Kokachev@gmail.com>
2020-07-14 13:55:54 +1000

    convert to dict of lists of sets

--- solver/main.py
+++ solver/main.py
@@ -1,3 +1,11 @@
+"""
+Data Structure
+dict = { 10: [{}, {}, {}, {}],
+         20: [{}]
+}
+
+"""
+
 
 def check_number_of_tasks(list_of_tasks):
     if len(list_of_tasks) < 3:
@@ -15,4 +23,11 @@ def sort_list_of_tasks(list_of_tasks):
     list_of_tasks.sort()
     return list_of_tasks
 
-print(check_number_of_tasks([1,2,3]))
+def convert_list_to_dict(list_of_tasks):
+    #Convert the list of tasks into appropriate data structure for the next step
+    D = {}
+    for i in range(len(list_of_tasks)):
+        print(i, list_of_tasks[i])
+        D[list_of_tasks[i]] = [set([i])]
+    return D
+    
--- test/test_solver.py
+++ test/test_solver.py
@@ -9,5 +9,11 @@ def test_integer_average_points():
     assert check_integer_average_points([1,1,1]) == "OK"
     assert check_integer_average_points([1,2,1]) == "Failed"
 
+#def test_at_least_one_set_of_tasks_exists():
+#    assert check_average_is_reachable([1,2,3,4,5,7,8]) == "OK"
+
 def test_sort():
     assert sort_list_of_tasks([1,10,1]) == [1,1,10]
+
+def test_convert_list_to_dict():
+    assert convert_list_to_dict([3,10,20]) == {3: [{0}], 10: [{1}], 20: [{2}]}
