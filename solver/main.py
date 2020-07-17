7422cd74ee38727ce632541bcedc97b8826c95d6
Eugene Kokachev <Eugene.Kokachev@gmail.com>
2020-07-17 14:40:09 +1000

    refactor tests + code

--- solver/main.py
+++ solver/main.py
@@ -4,70 +4,65 @@ dict = { 10: [{}, {}, {}, {}],
          20: [{}]
 }
 
-paths = [{Sum of Points: set(list of tasks that give that value)}]
-
-
+paths = [{Sum of Points: {subset of tasks that give that amount of points}}]
+List of dicts was chosen as it allows to store this mapping. 
+And data type set was chosen as dict value as tasks in the set are unique and sets make it easy to see if 2 of them have any common tasks. 
 """
-import bisect
 
-def check_number_of_tasks(list_of_tasks):
-    if len(list_of_tasks) < 3:
-        return "Failed"
-    else:
-        return "OK"
+class Task_Assigner: 
+    def __init__(self, list_of_tasks):
+        super().__init__()
+        self.list_of_tasks = list_of_tasks
+        self.check_number_of_tasks()
+        self.list_of_tasks.sort()
 
-def check_integer_average_points(list_of_tasks):
-    if (sum(list_of_tasks) % len(list_of_tasks)) == 0:
-        return "OK"
-    else:
-        return "Failed"
+        self.queue = self.convert_list_of_tasks_to_queue()
 
-def sort_list_of_tasks(list_of_tasks):
-    list_of_tasks.sort()
-    return list_of_tasks
+        self.average = self.calculate_average()
 
-def convert_list_to_dict(list_of_tasks):
-    #Convert the list of tasks into appropriate data structure for the next step
-    D = {}
-    for i in range(len(list_of_tasks)):
-        #print(i, list_of_tasks[i])
-        D[list_of_tasks[i]] = [set([i])]
-    return D
+        self.index_in_queue = -1
 
-def convert_list_of_tasks_to_paths(list_of_tasks):
-    D = []
-    for i in range(len(list_of_tasks)):
-        D.append({list_of_tasks[i]: {i}})
-    return D   
+    def calculate_average(self):
+        if (sum(self.list_of_tasks) % 3) == 0:
+            return sum(self.list_of_tasks) // 3
+        else:
+            raise ValueError("Average is not integer")
+
+    def check_number_of_tasks(self):
+        if len(self.list_of_tasks) < 3:
+            raise ValueError("There should be at least 3 tasks")
 
 
-def check_average_is_reachable(paths, list_of_tasks): 
-    #unique_elements = set(list_of_tasks)
-    queue = paths
-    AVERAGE = sum(list_of_tasks) // 3 #floor (integer) division
+    def convert_list_of_tasks_to_queue(self):
+        queue = []
+        for i in range(len(self.list_of_tasks)):
+            queue.append({self.list_of_tasks[i]: {i}})
+        return queue
 
-    index_in_queue = -1
-    while True: 
-        index_in_queue += 1
-        next_in_line = queue[index_in_queue]
-        
-        left_sum = next(iter(next_in_line)) # give the first key from a dict
-        left_path = next_in_line[left_sum]
 
-        if left_sum >= AVERAGE:
-            break
+    def generate_tasks_subsets(self):
+        while True: 
+            self.index_in_queue += 1
+            next_in_line = self.queue[self.index_in_queue]
+            
+            left_sum = next(iter(next_in_line)) # give the first key from a dict
+            left_path = next_in_line[left_sum]
 
-        for i in queue[index_in_queue+1:]:
-            right_sum = next(iter(i))
-            right_path = i[right_sum]
-            new_sum = left_sum + right_sum
-            if new_sum > AVERAGE:
+            if left_sum >= self.average:
                 break
-            if not bool(left_path & right_path): 
-                queue.append({new_sum: left_path.union(right_path)})
 
-
-    if AVERAGE in queue[index_in_queue]:
-        return "OK"
-    else:
-        return "Failed"
+            for i in self.queue[self.index_in_queue+1:]:
+                right_sum = next(iter(i))
+                right_path = i[right_sum]
+                new_sum = left_sum + right_sum
+                if new_sum > self.average:
+                    break
+                if not bool(left_path & right_path): 
+                    self.queue.append({new_sum: left_path.union(right_path)})
+                
+    def check_average_is_reachable(self): 
+        self.generate_tasks_subsets()
+        if self.average in self.queue[self.index_in_queue]:
+            return "OK"
+        else:
+            return "Failed"
--- test/test_solver.py
+++ test/test_solver.py
@@ -5,25 +5,29 @@ from solver.main import *
 COMPLICATED_SET = [2,2,2,3,3,3,4,4,4,5,5,5,10,11,12]
 
 def test_min_3_tasks():
-    assert check_number_of_tasks([1,1]) == "Failed"
+    with pytest.raises(ValueError) as excinfo:
+        assert Task_Assigner([1,1])
+    assert "at least 3 tasks" in str(excinfo.value)               
 
-def test_integer_average_points(): 
-    assert check_integer_average_points([1,1,1]) == "OK"
-    assert check_integer_average_points([1,2,1]) == "Failed"
+def test_calculate_average_points(): 
+    assert Task_Assigner([1,1,1]).average == 1
+    
+    with pytest.raises(ValueError) as excinfo:
+        assert Task_Assigner( [1, 2, 1] )
+
 
 def test_average_is_reachable():
-    p1 = convert_list_of_tasks_to_paths([9,9,12])
-    p2 = convert_list_of_tasks_to_paths([9,10,11])
-    p3 = convert_list_of_tasks_to_paths([1,2,3,4,5,7,8])
-    assert check_average_is_reachable(p1, [9,9,12]) == "Failed"
-    assert check_average_is_reachable(p2, [9,10,11]) == "OK"
-    assert check_average_is_reachable(p3, [1,2,3,4,5,7,8]) == "OK"
+    test_A = Task_Assigner([9,9,12])
+    test_B = Task_Assigner([9,10,11])
+    test_C = Task_Assigner([1,2,3,4,5,7,8])
+    assert test_A.check_average_is_reachable() == "Failed"
+    assert test_B.check_average_is_reachable() == "OK"
+    assert test_C.check_average_is_reachable() == "OK"
 
 def test_sort():
-    assert sort_list_of_tasks([1,10,1]) == [1,1,10]
-
-def test_convert_list_to_dict():
-    assert convert_list_to_dict([3,10,20]) == {3: [{0}], 10: [{1}], 20: [{2}]}
+    test_A = Task_Assigner([1,10,1])
+    assert test_A.list_of_tasks == [1,1,10]
 
 def test_create_initial_paths():
-    assert convert_list_of_tasks_to_paths([3,10,20]) == [{3: {0}}, {10: {1}}, {20: {2}}]
+    test_A = Task_Assigner([3,10,20])
+    assert test_A.queue == [{3: {0}}, {10: {1}}, {20: {2}}]
