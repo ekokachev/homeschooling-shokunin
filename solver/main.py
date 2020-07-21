7ed95fae25c8958bbeefb2bd338a4578b50128f9
Eugene Kokachev <Eugene.Kokachev@gmail.com>
2020-07-21 13:36:08 +1000

    new solution to make a complicated test pass

--- solver/main.py
+++ solver/main.py
@@ -8,6 +8,8 @@ paths = [{Sum of Points: {subset of tasks that give that amount of points}}]
 List of dicts was chosen as it allows to store this mapping. 
 And data type set was chosen as dict value as tasks in the set are unique and sets make it easy to see if 2 of them have any common tasks. 
 """
+#from bisect import bisect_left
+import uuid
 
 class Task_Assigner: 
     def __init__(self, list_of_tasks):
@@ -15,8 +17,8 @@ class Task_Assigner:
         self.list_of_tasks = list_of_tasks
         self.check_number_of_tasks()
         self.list_of_tasks.sort()
-
-        self.queue = self.convert_list_of_tasks_to_queue()
+        self.queue_sorted_keys = self.list_of_tasks
+        self.queue, self.mapping = self.convert_list_of_tasks_to_queue()
 
         self.average = self.calculate_average()
 
@@ -35,34 +37,73 @@ class Task_Assigner:
 
     def convert_list_of_tasks_to_queue(self):
         queue = []
+        mapping = {}
         for i in range(len(self.list_of_tasks)):
-            queue.append({self.list_of_tasks[i]: {i}})
-        return queue
+            unique_task_id = uuid.uuid4().fields[-1]
+            queue.append({self.list_of_tasks[i]: {unique_task_id}})
+            mapping[unique_task_id] = [i+1, self.list_of_tasks[i]]
+        return queue, mapping
 
 
     def generate_tasks_subsets(self):
-        while True: 
-            self.index_in_queue += 1
-            next_in_line = self.queue[self.index_in_queue]
+        for task_index in range(0, len(self.list_of_tasks)): 
+            #self.index_in_queue += 1
+            next_in_line = self.queue[task_index]
             
             left_sum = next(iter(next_in_line)) # give the first key from a dict
             left_path = next_in_line[left_sum]
 
-            if left_sum >= self.average:
-                break
-
-            for i in self.queue[self.index_in_queue+1:]:
+#            if left_sum >= self.average:
+#                break
+            
+            #frozen_queue = self.queue.copy()
+            queue_length = len(self.queue)
+            for i in self.queue[task_index + 1:queue_length]:
                 right_sum = next(iter(i))
                 right_path = i[right_sum]
                 new_sum = left_sum + right_sum
-                if new_sum > self.average:
-                    break
-                if not bool(left_path & right_path): 
+
+                if (not bool(left_path & right_path)) and (new_sum <= self.average): 
+#                    insert_index = bisect_left(self.queue_sorted_keys, new_sum)
+#                    self.queue_sorted_keys.insert(insert_index, new_sum)
+                    #self.queue.append({new_sum: left_path.union(right_path)})
                     self.queue.append({new_sum: left_path.union(right_path)})
                 
     def check_average_is_reachable(self): 
+        for item in self.queue:
+            if self.average in item:
+                return "OK"
+        return "Failed"
+
+    def convert_ids_to_weight(self, list_of_sets):
+        r = []
+        print(list_of_sets, '\n\n')
+        for s in list_of_sets:
+            new_s = [self.mapping[val][1] for val in s]
+            r.append(new_s)
+        print(self.mapping, '\n\n')
+        print(r)
+        return r
+
+    def assign_tasks(self):
         self.generate_tasks_subsets()
-        if self.average in self.queue[self.index_in_queue]:
-            return "OK"
+
+        candidates = []
+        for i in range(0, len(self.queue)):
+            item = self.queue[i]
+            if self.average in item:
+                candidates.append(item[self.average])
+
+        if len(candidates) < 3:
+            return "Failed"
         else:
+            for i in range(0, len(candidates)):
+                for j in range(i+1, len(candidates)): 
+                    for k in range(j+1, len(candidates)):
+                        A_tasks = candidates[i]
+                        B_tasks = candidates[j]
+                        C_tasks = candidates[k]
+                        if not ( bool(A_tasks & B_tasks) or bool(A_tasks & C_tasks) or bool(B_tasks & C_tasks)):
+                            self.assignments = self.convert_ids_to_weight([A_tasks, B_tasks, C_tasks])
+                            return "OK"
             return "Failed"
--- test/test_solver.py
+++ test/test_solver.py
@@ -20,14 +20,39 @@ def test_average_is_reachable():
     test_A = Task_Assigner([9,9,12])
     test_B = Task_Assigner([9,10,11])
     test_C = Task_Assigner([1,2,3,4,5,7,8])
+
+    test_A.generate_tasks_subsets()
     assert test_A.check_average_is_reachable() == "Failed"
+    test_B.generate_tasks_subsets()
     assert test_B.check_average_is_reachable() == "OK"
+    test_C.generate_tasks_subsets()
     assert test_C.check_average_is_reachable() == "OK"
 
 def test_sort():
     test_A = Task_Assigner([1,10,1])
     assert test_A.list_of_tasks == [1,1,10]
 
-def test_create_initial_paths():
+def test_convert_list_of_tasks_to_queue():
     test_A = Task_Assigner([3,10,20])
-    assert test_A.queue == [{3: {0}}, {10: {1}}, {20: {2}}]
+    assert len(test_A.mapping) == len(test_A.list_of_tasks)
+    assert len(test_A.queue) == len(test_A.list_of_tasks)
+
+def check_assignments(assignments, average): 
+    if sum(assignments[0]) == sum(assignments[1]) == sum(assignments[2]) == average:
+        return True
+    else:
+        return False
+
+def test_assign_tasks():
+    test_1 = Task_Assigner([9,9,12])
+    test_2 = Task_Assigner([9,10,11])
+    test_3 = Task_Assigner([2,2,2,1,1,1])
+    test_4 = Task_Assigner([1,2,3,4,5,7,8])
+    test_5 = Task_Assigner(COMPLICATED_SET)
+    assert test_1.assign_tasks() == "Failed"
+    assert test_2.assign_tasks() == "Failed"
+    assert test_3.assign_tasks() == "OK"
+    assert check_assignments(test_3.assignments, test_3.average)
+
+    assert test_5.assign_tasks() == "OK"
+    assert check_assignments(test_5.assignments, test_5.average)
